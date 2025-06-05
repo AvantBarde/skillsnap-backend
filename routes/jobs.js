330eadb1f18a678ae5dd8591b8d0c51da107784d
Bardell <Bardellwilson7@gmail.com>
2025-06-05 09:43:49 -0500

    Added express and routing once again to jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,4 +1,6 @@
+const express = require('express');
 const axios = require('axios');
+const router = express.Router();
 const https = require('https'); // ✅ Add this
 
 // 🔧 Create agent that skips SSL validation
